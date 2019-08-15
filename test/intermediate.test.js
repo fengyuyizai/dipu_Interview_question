d5c9a29635873e35ee2fada55bb6d194b6ad6caa
1942316144@qq.com <1942316144@qq.com>
2019-08-15 17:54:04 +0800

    修改测试代码

--- test/intermediate.test.js
+++ test/intermediate.test.js
@@ -47,42 +47,42 @@ describe('action is single situation, config[4 - 9]', () => {
   })
 })
 describe('action is batch situation, config[10 - end]', () => {
-  test(`test config[4], lack accessKeyID,accessKeySecret,accountName,toAddress,templateName,receiversName`, done => {
+  test(`test config[10], lack accessKeyID,accessKeySecret,accountName,toAddress,templateName,receiversName`, done => {
     function callback(err) {
       expect(err).toBe('accessKeyID required,accessKeySecret required,accountName required,templateName required,receiversName required')
       done()
     }
     intermediate(LocalData.config[10], callback)
   })
-  test(`test config[5], lack accessKeySecret, accountName, templateName, receiversName`, done => {
+  test(`test config[11], lack accessKeySecret, accountName, templateName, receiversName`, done => {
     function callback(err) {
       expect(err).toBe('accessKeySecret required,accountName required,templateName required,receiversName required')
       done()
     }
     intermediate(LocalData.config[11], callback)
   })
-  test(`test config[6], lack accountName, templateName, receiversName`, done => {
+  test(`test config[12], lack accountName, templateName, receiversName`, done => {
     function callback(err) {
       expect(err).toBe('accountName required,templateName required,receiversName required')
       done()
     }
     intermediate(LocalData.config[12], callback)
   })
-  test(`test config[7], lack templateName, receiversName`, done => {
+  test(`test config[13], lack templateName, receiversName`, done => {
     function callback(err, res) {
       expect(err).toBe('templateName required,receiversName required')
       done()
     }
     intermediate(LocalData.config[13], callback)
   })
-  test(`test config[7], lack receiversName`, done => {
+  test(`test config[14], lack receiversName`, done => {
     function callback(err, res) {
       expect(err).toBe('receiversName required')
       done()
     }
     intermediate(LocalData.config[14], callback)
   })
-  test(`test config[7], The request is illegal, param err`, done => {
+  test(`test config[15], The request is illegal, param err`, done => {
     function callback(err, res) {
       // 因为服务器代理不上，此处无法测出服务器真实返回值
       expect(res.isErr).toBe(false)
